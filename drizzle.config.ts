81ebc1b31d867563ec6c89d5aec2b7b066a65138
qiuyulc <qiuyulc@gmail.com>
2025-04-07 10:38:43 +0800

    修改配置

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,10 @@
 import 'dotenv/config';
-import { defineConfig } from 'drizzle-kit';
-export default defineConfig({
+import type { Config } from 'drizzle-kit';
+
+export default {
   out: './drizzle',
   schema: './src/db/schema.ts',
-  dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    connectionString: process.env.DATABASE_URL!,
   },
-});
+} satisfies Config;
